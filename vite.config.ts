3b87d5401f23ff3500144234c0f1696a0a458a45
pixalynxdev <pixalynx@gmail.com>
2024-01-16 21:15:51 +1100

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,7 @@ import dts from "vite-plugin-dts";
 // https://vitejs.dev/config/
 export default defineConfig({
   resolve: {
+    preserveSymlinks: true,
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
